c1a8f3165f3ea885ae98bed5657541875b1f4940
fiachmchh <fiachmchh@gmail.com>
2021-02-12 12:39:00 +0000

    seeds.sql file added

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (name) VALUES ('Chicken');
+INSERT INTO burgers (name) VALUES ('Fish');
+INSERT INTO burgers (name) VALUES ('Double Whopper');
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheese', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (name, devoured) VALUES ('Lamb', true);
+
+   
