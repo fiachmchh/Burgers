9ee1a00b162710e208b8cee758c553cdad87f4a2
fiachmchh <fiachmchh@gmail.com>
2021-02-13 00:44:30 +0000

    routes working a burgers posting

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static('public'));
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// // Parse application body as JSON
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 
 // Set Handlebars.
 const exphbs = require('express-handlebars');
