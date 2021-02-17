3d0652a624c1f3dbd3c1b885aaa42fb2d65df4cb
fiachmchh <fiachmchh@gmail.com>
2021-02-17 03:55:58 +0000

    jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'chvfeevd829',
-  database: 'burgers_db',
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.creatConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    // NOTE: Be sure to add your MySQL password here!
+    password: 'chvfeevd829',
+    database: 'burgers_db',
+  });
+};
+
 
 // Make connection.
 connection.connect((err) => {
