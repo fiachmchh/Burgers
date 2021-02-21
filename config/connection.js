59141ed45b7821c9d3852efd91f2e262ec5b88d0
fiachmchh <fiachmchh@gmail.com>
2021-02-21 15:33:03 -0500

    spell correct

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.creatConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
@@ -26,3 +26,4 @@ connection.connect((err) => {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
